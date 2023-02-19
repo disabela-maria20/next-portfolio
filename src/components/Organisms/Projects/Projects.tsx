ea40a1db988421a0d1080f0ae9c5d69228241189
Isabela Maria <isabela10014@hotmail.com>
2023-02-19 18:55:03 -0300

    feat: :sparkles: project section update

--- src/components/Organisms/Projects/Projects.tsx
+++ src/components/Organisms/Projects/Projects.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { lazy, Suspense, useState } from 'react'
+
+import React, { Suspense, useState } from 'react'
 import { Title } from '../../Atoms'
 import server from '../../../json/ServerProjescts.json'
+import Link from 'next/link'
 
-const Project = React.lazy(() => import('../../Templates/THome/THome'))
 
 const Projects: React.FC = (): JSX.Element => {
   const [select, setSelect] = useState('professional');
@@ -14,28 +14,30 @@ const Projects: React.FC = (): JSX.Element => {
         <Title>
           Trabalhos
         </Title>
-        <select className='px-10 py-10 border rounded-md w-164' defaultValue="professional" value={select ? select : ''} onChange={({ target }) => setSelect(target.value)}>
-          <option value="professional" selected>Profissional</option>
+        <select className='px-10 py-10 border rounded-md w-164' defaultValue="professional" onChange={({ target }) => setSelect(target.value)}>
+          <option value="professional">Profissional</option>
           <option value="study">Estudo</option>
         </select>
       </div>
 
-      <Suspense fallback={<div>teste</div>}>
-        <article className='grid grid-cols-1 grid-rows-6 md:grid-rows-none sm:grid-cols-2 md:grid-cols-3 gap-26 md:gap-38'>
-          {server
-            .filter((e) => e.type === select)
-            .map(data => (
-              <div key={data.id} className="relative z-20">
-                <img src={data.img} alt={`Projeto ${data.name}`} width="auto" height="auto" className=' relative -z-10 h-full object-cover' />
-                <div className='absolute top-0 h-full w-full flex justify-center flex-col items-center bg-black bg-opacity-60 hover:blur-sm'>
-                  <h3 className='text-white font-sans font-semibold text-18 hover:blur-none'>{data.name}</h3>
-                  <img src={data.technology.icon} alt={`Projeto ${data.technology.name}`} width="auto" height="auto" className='relative -z-10 object-cover  w-22' />
-                </div>
-              </div>
-            ))}
-        </article>
-      </Suspense>
 
+      <article className='grid grid-cols-1 grid-rows-6 md:grid-rows-none sm:grid-cols-2 md:grid-cols-3 gap-26 md:gap-26'>
+        {server
+          .filter((e) => e.type === select)
+          .map(data => (
+            <div key={data.id} className="relative z-20 block-project">
+              <Link href={`${data.link}`}>
+                <a rel='noreferrer nofollow noopener' target="_SEJ">
+                  <img src={data.img} alt={`Projeto ${data.name}`} width="auto" height="auto" className='block-project-img relative -z-10 h-full object-cover' />
+                  <div className='absolute top-0 h-full w-full flex justify-center flex-col items-center'>
+                    <h3 className='text-white font-sans font-semibold text-18'>{data.name}</h3>
+                    <img src={data.technology.icon} alt={`Projeto ${data.technology.name}`} width="auto" height="auto" className='relative  -z-10 object-cover w-22' />
+                  </div>
+                </a>
+              </Link>
+            </div>
+          ))}
+      </article>
     </div>
   )
 }
